f10c0b06a6f40c506f00716029bbbe19b51db7b6
Roman Cortes <javier.roman.c@gmail.com>
2021-11-11 20:47:43 -0500

    Fix install path

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 require("fs").copyFile(
   __dirname + "/launcher.html",
-  __dirname + "../../launcher.html", 
+  __dirname + "/../../launcher.html", 
   error => {
     if (error) {
       throw error
